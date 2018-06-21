b356bad9cdc47c3ce96c6573fa454a3bf1c50854
Mert <merterden97@gmail.com>
2018-06-21 13:09:59 -0400

    Made Auth

--- testrequests.py
+++ testrequests.py
@@ -5,53 +5,62 @@ import six
 import six.moves.urllib.parse as urllibparse
 import webbrowser
 import base64
+import urllib.request
 
-RURI = 'https://example.com/callback/'
+# RURI = 'https://example.com/callback/'
 
-CLIENTID = 'abdd03cd5c1c4dc79d15cbf50b0641ad'
+# CLIENTID = 'abdd03cd5c1c4dc79d15cbf50b0641ad'
 
-CSE = '5b1d951d01464ccea685a5fc35977d33'
+# CSE = '5b1d951d01464ccea685a5fc35977d33'
 
+
+class SpotAuth():
+
+    def __init__(self, clientid=None, clientsecret=None, redirect=None):
+        self.clientid = clientid
+        self.clientsecret = clientsecret
+        self.redirect = redirect
+        self.token = self._token()
+
+    def _token(self):
+
+        url = auth_url(self.clientid, self.redirect)
+        print("You Are Being Redirected to login to Spotfiy")
+        print("Please Copy and Paste Redirected Link")
+        webbrowser.open(url)
+        code = input()
+        response = get_token(response=code, client_id=self.clientid, redirect_uri=self.redirect, secret=self.clientsecret)
+        print(response)
+        return response.json()['access_token']
+
+    
 def auth_url(client_id, redirect_uri):
 
     packet = {'client_id': client_id,
                    'response_type': 'code',
-                   'redirect_uri': redirect_uri}
+                   'redirect_uri': redirect_uri,
+                   'scope': 'user-read-private user-modify-playback-state'+\
+            ' user-read-recently-played user-read-playback-state'+\
+            ' playlist-read-private playlist-modify-public'+\
+            ' user-read-currently-playing user-top-read playlist-modify-private '+\
+            ' playlist-read-collaborative'}
     params = urllibparse.urlencode(packet)
     url = 'https://accounts.spotify.com/authorize?' + params
     return url
 
-def get_token(response, client_id, redirect_uri):
+def get_token(response=None, client_id=None, redirect_uri=None, secret=None):
 
     r = response.split("?code=")[1].split("&")[0]
-    
     packet = {'grant_type': 'authorization_code',
                    'code': r,
-                   'redirect_uri': RURI}
+                   'redirect_uri': redirect_uri}
     
-    
-    b = CLIENTID + ':' + CSE
-    print(b)
+    b = client_id + ':' + secret
     encoded = base64.b64encode(six.text_type(b).encode('ascii'))
-    print(encoded.decode('ascii'))
-    #encoded = encoded.decode('ascii')
-    #print(encoded)
     auth = 'Basic ' + str(encoded.decode('ascii'))
     header = {'Authorization': auth}
-    auth_header = base64.b64encode(six.text_type(CLIENTID + ':' + CSE).encode('ascii'))
-    h = {'Authorization': 'Basic ' + auth_header.decode('ascii')}
-    r = requests.post('https://accounts.spotify.com/api/token', data=packet,headers=h, verify=True)
+    r = requests.post('https://accounts.spotify.com/api/token', data=packet,headers=header, verify=True)
     return r
 
-url = auth_url('abdd03cd5c1c4dc79d15cbf50b0641ad','https://example.com/callback/')
-webbrowser.open(url)
 
-response = input("Input URL\n")
-
-r = get_token(response, CLIENTID, RURI)
-
-
-
-# url = "https://accounts.spotify.com/authorize"
-# info = {"client_id":"abdd03cd5c1c4dc79d15cbf50b0641ad", "redirect_uri": 'https://example.com/callback/', 'response_type': 'code'}
-# r = requests.get(url, params=info)
+s = SpotAuth(clientid='abdd03cd5c1c4dc79d15cbf50b0641ad', clientsecret='5b1d951d01464ccea685a5fc35977d33', redirect='https://example.com/callback/')
